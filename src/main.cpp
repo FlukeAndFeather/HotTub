c44fdebbd918ee311b0b9e29ed466376eeea9fdf
Max Czapanskiy <maxczap@stanford.edu>
2019-09-03 13:40:41 -0700

    Reading thermometer

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+
+#define SUBTEMP_PIN 4
+
+void setup() {
+  // Connect to Serial
+  Serial.begin(9600);
+  while(!Serial);
+
+  pinMode(SUBTEMP_PIN, INPUT);
+}
+
+void loop() {
+  
+  // Read temperature
+  int16_t subtemp_raw = analogRead(SUBTEMP_PIN);
+
+  // Convert to degC
+  int16_t mask = 0x07FF;
+  float subtemp_degC = (subtemp_raw & mask) / 16.0;
+
+  // Print raw and converted values
+  Serial.print("Raw value: ");
+  Serial.println(subtemp_raw, BIN);
+  Serial.print("Deg C: ");
+  Serial.println(subtemp_degC, 4);
+  delay(1000);
+}
